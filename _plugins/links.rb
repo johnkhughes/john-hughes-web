2399a999b74905fc30472d0ffe3e1000131091c8
John Hughes <john@hughesblog.co.uk>
2015-08-29 12:50:16 +0100

    Show external links in index

---
+++ _plugins/links.rb
@@ -0,0 +1,11 @@
+module Jekyll
+  module ExtractUrlFilter
+
+    def extract_url(content)
+      /<a\s[^>]*?href=(["'])(.+?)\1/i.match(content) { |m| m[2] }
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ExtractUrlFilter)
