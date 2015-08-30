c46d6d7dea50b17b02e6cf84746db398a9c1800d
John Hughes <john@hughesblog.co.uk>
2015-08-30 10:30:06 +0100

    Make JS work without classList

--- static/js/common.js
+++ static/js/common.js
@@ -2,20 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var nav = document.querySelector('.nav');
     var button = document.createElement('a');
-    var hidden = 'hidden';
-
-    if (!('classList' in nav)) {
-        return;
-    }
+    var visible = nav.className;
+    var hidden = visible + ' hidden';
 
     button.addEventListener('click', function(e) {
         e.preventDefault();
-        nav.classList.toggle(hidden);
+        if (nav.className === visible) {
+            nav.className = hidden;
+        } else {
+            nav.className = visible;
+        }
     });
 
     button.innerHTML = 'Menu';
     button.href = '#';
     nav.insertBefore(button, nav.firstChild);
-    nav.classList.add(hidden);
+    nav.className = hidden;
 
 });
